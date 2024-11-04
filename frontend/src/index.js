51b506bb1038b08e632799c3a6a1bf8f84b9fc69
minhtai <21102003tai@gmail.com>
2024-11-04 21:08:04 +0700

    add '@fortawesome/fontawesome-free/css/all.min.css'; package

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BookContextProvider } from './context/BookContext';
+
 
 
 import { Provider } from "react-redux"
