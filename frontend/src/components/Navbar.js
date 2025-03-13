81539a8b55140647ae41fafb5a72fdbcb2338e21
tnthulai <118844603+tnthulai@users.noreply.github.com>
2025-03-13 21:47:33 +0700

    commitcommit

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -70,44 +70,9 @@ const Navbar = () => {
                                 )}
                             </div>
                         </div>
-<<<<<<< HEAD
-                        <div className="container px-0">
-                            <nav className="navbar navbar-light bg-white navbar-expand-xl">
-                                <a href="/" className="navbar-brand "><h1 className=" display-6 logo"><i className="fas fa-book-open"></i>BOOKSHOP.VN</h1></a>
-
-                                <button className="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                                    <span className="fa fa-bars text-primary"></span>
-                                </button>
-                                <div className="collapse navbar-collapse bg-white" id="navbarCollapse">
-                                    <div className="navbar-nav mx-auto">
-                                        <Link to="/" className="nav-item nav-link active fw-bold fs-5">Trang chủ</Link>
-                                        <Link to="/" className="nav-item nav-link fw-bold fs-5">Mượn Sách</Link>
-                                        <Link to="/exchange" className="nav-item nav-link fw-bold fs-5">Trao Đổi Sách</Link>
-                                        <Link to="/contact" className="nav-item nav-link fw-bold fs-5">Liên Lạc</Link>
-                                    </div>
-                                    <div className="d-flex m-3 me-0">
-
-                                        <Link to={'/cart'} className="position-relative me-4 my-auto">
-                                            <i class="fa fa-shopping-cart fa-2x cart-nav" aria-hidden="true"></i>
-                                            
-
-                                        </Link>
-                                        <Link to={'/auth'} className="my-auto">
-                                            {/* <i className="fas fa-user fa-2x"></i> */}
-                                            <button type="button" class="btn btn-primary">Đăng nhập</button>
-                                        </Link>
-                                    </div>
-                                </div>
-                            </nav>
-                        </div>
-                    </div>
-                )
-            }
-=======
                     </nav>
                 </div>
             </div>
->>>>>>> c685fe1 (add book exchange page)
         </div>
     )
 }
