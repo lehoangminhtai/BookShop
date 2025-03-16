03b0de9bba39549e8a54908c311ce834fc049e54
minhtai <21102003tai@gmail.com>
2025-03-17 03:03:26 +0700

    edit my post page and

--- frontend/src/pages/exchange/MyPost.js
+++ frontend/src/pages/exchange/MyPost.js
@@ -1,38 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+//context
+import { useStateContext } from "../../context/UserContext";
 //scss
 import '../../css/user/MyPost.scss'
+//services
+import { getBookExchangesByUser } from "../../services/exchange/bookExchangeService";
 
 const MyPosts = () => {
-    const [posts, setPosts] = useState([
-        {
-            id: 1,
-            title: "Sách lập trình JavaScript",
-            image: "https://m.media-amazon.com/images/I/71787OImkEL.jpg",
-            date: "2024-02-10",
-            location: "Hồ Chí Minh",
-            status: "pending"
-        },
-        {
-            id: 2,
-            title: "Bộ truyện Harry Potter",
-            image: "https://img.posterstore.com/zoom/wb0001-8harrypotter-sorcerersstone50x70.jpg",
-            date: "2024-02-08",
-            location: "Hà Nội",
-            status: "processing"
-        },
-        {
-            id: 3,
-            title: "Sách kinh tế học vĩ mô",
-            image: "https://hvpnvn.edu.vn/wp-content/uploads/sites/63/2024/02/26102016120000-0001.jpg",
-            date: "2024-02-06",
-            location: "Đà Nẵng",
-            status: "completed"
-        }
-    ]);
+    
+    const [posts, setPosts] = useState([]);
 
     const navigate = useNavigate();
+
+    const { user } = useStateContext();
+    const userId = user?._id;
+
+    const fetchPosts = async () => {
+        try {
+            const response = await getBookExchangesByUser(userId);
+            if(response.data.success){
+                setPosts(response.data.bookExchanges);
+            }
+        } catch (error) {
+            console.log("Failed to fetch posts: ", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, [userId]);
 
     const handleClickPost = (postId) => {
         navigate(`/exchange-post-detail/${postId}`);
@@ -40,7 +38,7 @@ const MyPosts = () => {
 
     const getStatusBadge = (status) => {
         switch (status) {
-            case "pending":
+            case "available":
                 return <span className="status-badge status-pending">⏳ Đang đợi trao đổi</span>;
             case "processing":
                 return <span className="status-badge status-processing">🔄 Đang xử lý</span>;
@@ -64,29 +62,28 @@ const MyPosts = () => {
             <h2 className="h2 text-center mt-5 mb-5 text-primary">-- <span className=""> Bài đăng của tôi </span> --</h2>
             <div className="row">
                 {posts.map((post) => (
-                    <div key={post.id} className="col-md-6 col-lg-4 mb-4">
+                    <div key={post?._id} className="col-md-6 col-lg-4 mb-4">
                         <div
                             className="card p-3 shadow-lg card-custom"
-                            onClick={() => handleClickPost(post.id)}
+                            onClick={() => handleClickPost(post?._id)}
                             style={{ cursor: "pointer" }}
                         >
                             <div className="d-flex">
                                 <img
-                                    src={post.image}
+                                    src={post?.images[0]}
                                     className="img-fluid card-img-custom me-3"
-                                    alt={post.title}
+                                    alt={post?.title}
                                 />
                                 <div className="flex-grow-1">
                                     <h3 className="h6 text-truncate-2 fw-bold text-dark">
-                                        {post.title}
+                                        {post?.title}
                                     </h3>
                                     <p className="text-muted mb-1">
-                                        📅 Ngày đăng: {post.date}
+                                        📅 Ngày đăng: {new Date(post?.createdAt).toLocaleDateString('vi-VN')}
                                     </p>
-                                    <p className="text-muted">
-                                        📍 {post.location}
-                                    </p>
-                                    <div className="mt-2">{getStatusBadge(post.status)}</div>
+                                    
+                                    <div className="mt-2">{getStatusBadge(post?.status)}</div>
+                                   
                                 </div>
                             </div>
                         </div>
