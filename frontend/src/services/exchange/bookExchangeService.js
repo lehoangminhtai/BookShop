cd3a223f9a3f53a5f269357b44b24e109f37bcf9
minhtai <21102003tai@gmail.com>
2025-03-17 02:59:27 +0700

    Create book exchange service

---
+++ frontend/src/services/exchange/bookExchangeService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { serverUrl } from "../config";
+
+const API = axios.create({ baseURL: `${serverUrl}/api/book-exchange` });
+
+export const getBookExchanges = async (page, limit) => {
+    try {
+        const res = await API.get(`/?page=${page}&limit=${limit}`);
+        return res.data;
+    } catch (error) {
+        return { success: false, message: error.message
+        };
+    }
+}
+
+export const getBookExchangeSer = async (bookExchangeId) => await API.get(`/${bookExchangeId}`);
+
+export const createBookExchange = async (bookData) => await API.post('/', bookData);
+
+export const updateBookExchange = async (id, bookData) => await API.put(`/${id}`, bookData);
+
+export const deleteBookExchange = async (id) => await API.delete(`/${id}`);
+
+export const getBookExchangesByUser = async (userId) => await API.get(`/user/${userId}`);
+
+export const getBookExchangesByCategory = async (category) => await API.get(`/category/${category}`);
