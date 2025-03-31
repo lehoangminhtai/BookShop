21eab865dc68fc710d669d0b54d6fd7c490b9807
tnthulai <118844603+tnthulai@users.noreply.github.com>
2025-03-31 16:42:22 +0700

    update exchange infor

--- frontend/src/services/exchange/exchangeInforService.js
+++ frontend/src/services/exchange/exchangeInforService.js
@@ -5,3 +5,16 @@ const API = axios.create({ baseURL: `${serverUrl}/api/exchange-infor` });
 
 
 export const createExchangeInforSer = async (data) => await API.post('/',data)
+
+export const getExchangeInforSer = async (requestId) =>{
+    try {
+        const res = await API.get(`/${requestId}`);
+        return res.data;
+    } catch (error) {
+        console.error('Error fetching exchange information:', error);
+        throw error;
+    }
+}
+
+export const updateExchangeInforSer = async (data) => await API.put(`/update/${data.requestId}`,data)
+     
